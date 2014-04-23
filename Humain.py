1bf290cac4f620c86352241727ffc802a0297042
fireraccoon <willyjames.music@gmail.com>
2014-04-23 09:25:12 -0400

    Update Humain.py

--- Humain.py
+++ Humain.py
@@ -2,8 +2,6 @@
 class Humain:
     def __init__(self, modele):
         self.modele = modele
-        self.force_attaque_basique = 10
-        self.nbr_vaisseaux_par_attaque = 5
         self.planeteMere = None
 
   
