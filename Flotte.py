ad5bbd00a8e84c419f9a117ed8734ae39ba71c1f
unknown <r2FOURNST@529W16.decinfo.cvm>
2014-04-23 08:25:23 -0400

    Ajout de planete+flotte+humain

--- Flotte.py
+++ Flotte.py
@@ -5,6 +5,7 @@ class Flotte:
         self.positionDepart  # planete d'ou partent les vaisseaux
         self.positionArrivee # planete ou vont les vaisseaux
         self.nbVaisseaux     # nombre de vaisseaux en deplacement vers une planete ciblee
-        self.tempsDepart     # moment en annee et dixieme x.x du depart
+        self.tempsDepart     # moment en annees et dixieme x.x du moment du depart des vaisseaux
         self.tempsArrivee    # moment de l'arrivee x.x de l'arrivee sur la planete ciblee
         
+    
