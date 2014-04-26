16543629e861623238aa84fb3a7e124087176a2f
patenteapoil <liam.giverin@gmail.com>
2014-04-26 14:50:36 -0400

    remplacement de mauvaises variables
    temps_courant et tempsCourant emplacées par anneeCourante

--- Gubru.py
+++ Gubru.py
@@ -11,8 +11,8 @@ class Gubru:
         self.listePlanetesAttaquees = []
         
     def forceAttaque(self):
-        if self.parent.temps_courant * self.nbr_vaisseaux_par_attaque + self.force_attaque_basique > self.force_attaque_basique*2:
-            return self.parent.temps_courant * self.nbr_vaisseaux_par_attaque + self.force_attaque_basique
+        if self.parent.anneeCourante * self.nbr_vaisseaux_par_attaque + self.force_attaque_basique > self.force_attaque_basique*2:
+            return self.parent.anneeCourante * self.nbr_vaisseaux_par_attaque + self.force_attaque_basique
         else : 
             return self.force_attaque_basique*2
         
